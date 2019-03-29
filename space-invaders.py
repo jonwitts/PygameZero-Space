83cee85249effbec84501b2c3cfd65b3ac62fc93
JOn Witts <jonwitts@gmail.com>
2019-03-29 15:01:02 +0000

    add second alien

--- space-invaders.py
+++ space-invaders.py
@@ -2,31 +2,38 @@ import random
 
 # define our sprites
 alien = Actor('alien')
+alien2 = Actor('alien2')
 rocket = Actor('rocket')
 laser = Actor('laser')
 
 # set our stage
 WIDTH = 800
-HEIGHT = 500
+HEIGHT = 800
 
 # position our sprites 
 rocket.midbottom = WIDTH / 2, HEIGHT - 5
 alien.midbottom = (random.randint(40, WIDTH - 40), 80)
+alien2.midbottom = (random.randint(40, WIDTH - 40), 80)
 
 # set score and statuses 
 score = 0
-game_status = 0 # 0 = stop, 1 = playing
+game_status = 0  # 0 = stop, 1 = playing
 laser_status = 0  # 0 for not fired, 1 for fired
+alien_speed = 1.0
+laser_speed = 1.0
 
 def draw():
     if game_status == 0:
         screen.draw.text("Press F1", (100, 300), color="white", fontsize=32)
     if game_status == 1:
         screen.clear()
-        screen.draw.text("score: " + str(score), (20, 20))
+        screen.draw.text("Score: " + str(score), (20, 20))
         rocket.draw()
-        move_alien()  # move our alien down each draw loop
+        move_alien(alien)  # move our alien down each draw loop
         alien.draw()
+        if score > 10:
+            move_alien(alien2)
+            alien2.draw()
         if laser_status > 0:
             move_laser()
             laser.draw()
@@ -52,25 +59,40 @@ def checkKeys():
                 laser.pos = (rocket.x, HEIGHT - 98)
                 laser_status = 1
 
-def move_alien():
-    alien.pos = (alien.x, alien.y+1)
+def move_alien(alien_name):
+    global alien_speed
+    alien_name.pos = (alien_name.x, alien_name.y+(1*alien_speed))
     
 def move_laser():
-    global laser_status
+    global laser_status, laser_speed
     if laser_status > 0:
-        laser.pos = (laser.x, laser.y - 2)
+        laser.pos = (laser.x, laser.y - (2*laser_speed))
         if laser.y < 10:
             laser_status = 0
 
-def detect_hits():
-    global game_status, score, laser_status
-    if alien.y >= HEIGHT - 40:
+def game_over(alien_name):
+    global game_status
+    if alien_name.y >= HEIGHT - 40:
         screen.draw.text("Game Over", (100, 300), color="red", fontsize=32)
         game_status = 1
-    if laser_status == 1 and alien.collidepoint(laser.pos):
-        hit_alien()
+
+def laser_hit(alien_name, aggression, powerup):
+    ''' Each alien can speed up with aggression and give you a '''
+    ''' quicker laser with power up '''
+    global laser_status, score, alien_speed, laser_speed
+    if laser_status == 1 and alien_name.collidepoint(laser.pos):
+        hit_alien(alien_name)
         laser_status = 0
         score += 1
+        alien_speed += aggression
+        laser_speed += powerup
 
-def hit_alien():
-    alien.midbottom = (random.randint(40, WIDTH - 40), 80)
+def detect_hits():
+    global score, laser_status, alien_speed, laser_speed
+    game_over(alien)
+    game_over(alien2)
+    laser_hit(alien, 0.125, 0.1)
+    laser_hit(alien2, 0.1, 0.125)
+
+def hit_alien(alien_name):
+    alien_name.midbottom = (random.randint(40, WIDTH - 40), 80)
